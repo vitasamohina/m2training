1b069a493b29d8d4fc286acb442ad4b48bb5bcf6
vita <vitasamohina89@gmail.com>
2022-06-25 00:38:46 +0300

    1.3

---
+++ app/code/Training/AdditionalTestOM/Model/ManagerCustom.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Training\AdditionalTestOM\Model;
+class ManagerCustom implements \Training\TestOM\Model\ManagerInterface
+{
+    public function create()
+    {}
+    public function get()
+    {}
+}
