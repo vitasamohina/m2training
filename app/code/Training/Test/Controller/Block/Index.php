740b869ea476cf512956b47c8c6fd811891fde3d
vita <vitasamohina89@gmail.com>
2022-07-07 23:07:19 +0300

    3.3

--- app/code/Training/Test/Controller/Block/Index.php
+++ app/code/Training/Test/Controller/Block/Index.php
@@ -10,17 +10,23 @@ class Index extends \Magento\Framework\App\Action\Action
      * @param \Magento\Backend\App\Action\Context $context
      * @param \Magento\Framework\View\LayoutFactory $layoutFactory
      */
+
+    private $resultRawFactory;
     public function __construct(
         \Magento\Backend\App\Action\Context $context,
-        \Magento\Framework\View\LayoutFactory $layoutFactory
+        \Magento\Framework\View\LayoutFactory $layoutFactory,
+         \Magento\Framework\Controller\Result\RawFactory $resultRawFactory
     ) {
         $this->layoutFactory = $layoutFactory;
+        $this->resultRawFactory = $resultRawFactory;
         parent::__construct($context);
     }
     public function execute()
     {
         $layout = $this->layoutFactory->create();
         $block = $layout->createBlock('Training\Test\Block\Test');
-        $this->getResponse()->appendBody($block->toHtml());
+        //$this->getResponse()->appendBody($block->toHtml());
+        $resultRaw = $this->resultRawFactory->create();
+        return $resultRaw->setContents($block->toHtml());
     }
 }
