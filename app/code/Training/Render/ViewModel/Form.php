bda19ae2e82ecb9b8f8119e7b303df28fa56fc88
vita <vitasamohina89@gmail.com>
2022-07-08 23:16:01 +0300

    3.14

---
+++ app/code/Training/Render/ViewModel/Form.php
@@ -0,0 +1,24 @@
+<?php
+namespace Training\Render\ViewModel;
+class Form implements \Magento\Framework\View\Element\Block\ArgumentInterface
+{
+    /**
+     * @var \Magento\Framework\UrlInterface
+     */
+    private $urlBuilder;
+
+    /**
+     * @param \Magento\Framework\UrlInterface $urlBuilder
+     */
+    public function __construct(
+        \Magento\Framework\UrlInterface $urlBuilder
+    )
+    {
+        $this->urlBuilder = $urlBuilder;
+    }
+
+    public function getSubmitUrl()
+    {
+        return $this->urlBuilder->getUrl('customer/account/login');
+    }
+}
