2a52f8ba0852bf87469d68a0f385c4c9066e3209
vita <vitasamohina89@gmail.com>
2022-07-14 19:40:48 +0300

    4.3

--- app/code/Training/Js/view/frontend/requirejs-config.js
+++ app/code/Training/Js/view/frontend/requirejs-config.js
@@ -3,6 +3,9 @@ var config = {
         mixins: {
             'Magento_Catalog/js/catalog-add-to-cart': {
                 'Training_Js/js/catalog-add-to-cart/mixin': true
+            },
+            'Magento_Checkout/js/action/place-order': {
+                'Training_Js/js/checkout/action/place-order/mixin': false
             }
         }
     }
